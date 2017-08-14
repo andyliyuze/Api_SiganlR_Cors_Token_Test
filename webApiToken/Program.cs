529a5f5c9661c5b070f62bdc2704ee004bec988e
andyliyuze <874787504@qq.com>
2017-08-14 12:29:20 +0800

    将Api服务使用owin，self-host

---
+++ webApiToken/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webApiToken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string baseAddress = "http://localhost:50046/";
+
+            //// Start OWIN host
+            //using (WebApp.Start<Startup>(url: baseAddress))
+            //{
+            //    // Create HttpCient and make a request to api/values
+            //    //HttpClient client = new HttpClient();
+
+            //    //HttpResponseMessage response = client.GetAsync(baseAddress + "api/values").Result;
+
+            //    Console.WriteLine("启动监听");
+            //    //Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            //}
+            const string uri = "http://localhost:50046/";
+
+            using (WebApp.Start(uri, Startup.Configuration))
+            {
+                Console.WriteLine("Started listening on " + uri);
+                Console.ReadLine();
+                Console.WriteLine("Shutting down...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ webApiToken/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using System.Web.Http.Cors;
+[assembly: OwinStartup(typeof(webApiToken.Startup))]
+
+namespace webApiToken
+{
+    public class Startup
+    {
+        public static void Configuration(IAppBuilder appBuilder)
+        {
+            
+            // 有关如何配置应用程序的详细信息，请访问 http://go.microsoft.com/fwlink/?LinkID=316888
+            // Configure Web API for Self-Host
+            HttpConfiguration config = new HttpConfiguration();
+            //跨域配置
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
